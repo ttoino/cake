593c717c1a8ca161e882469276fa5fa43ac1e7c3
toino <me@toino.pt>
2025-08-07 05:20:15 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "My Awesome Desktop Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ags,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pname = "cake";
+    entry = "app.ts";
+
+    extraPackages = with ags.packages.${system}; [
+      apps
+      astal4
+      battery
+      bluetooth
+      hyprland
+      io
+      mpris
+      network
+      notifd
+      powerprofiles
+      wireplumber
+    ];
+  in {
+    packages.${system} = {
+      default = pkgs.stdenv.mkDerivation {
+        name = pname;
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          wrapGAppsHook
+          gobject-introspection
+          ags.packages.${system}.default
+        ];
+
+        buildInputs = extraPackages ++ [pkgs.gjs];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/bin
+          mkdir -p $out/share
+          cp -r * $out/share
+          ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
+
+          runHook postInstall
+        '';
+      };
+    };
+
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        buildInputs = [
+          (ags.packages.${system}.default.override {
+            inherit extraPackages;
+          })
+        ];
+
+        packages = with pkgs; [
+          nodejs
+          corepack
+        ];
+      };
+    };
+  };
+}
