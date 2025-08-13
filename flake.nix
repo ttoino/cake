e5605f694203f9cad3c9e73651d9e438e99a4ffd
toino <me@toino.pt>
2025-08-13 15:10:25 +0100

    Improve flake config and typecheck action

--- flake.nix
+++ flake.nix
@@ -10,42 +10,48 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ags,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    pname = "cake";
-    entry = "app.ts";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ags,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pname = "cake";
+      entry = "app.ts";
 
-    extraPackages = with ags.packages.${system}; [
-      apps
-      astal4
-      battery
-      bluetooth
-      hyprland
-      io
-      mpris
-      network
-      notifd
-      powerprofiles
-      wireplumber
-    ];
-  in {
-    packages.${system} = {
-      default = pkgs.stdenv.mkDerivation {
+      ags-bin =
+        with ags.packages.${system};
+        (default.override {
+          extraPackages = [
+            apps
+            astal4
+            battery
+            bluetooth
+            hyprland
+            io
+            mpris
+            network
+            notifd
+            powerprofiles
+            wireplumber
+          ];
+        });
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
         name = pname;
         src = ./.;
 
         nativeBuildInputs = with pkgs; [
-          wrapGAppsHook
+          ags-bin
           gobject-introspection
-          ags.packages.${system}.default
+          wrapGAppsHook
         ];
 
-        buildInputs = extraPackages ++ [pkgs.gjs];
+        buildInputs = [ pkgs.gjs ];
 
         installPhase = ''
           runHook preInstall
@@ -58,21 +64,23 @@
           runHook postInstall
         '';
       };
-    };
 
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        buildInputs = [
-          (ags.packages.${system}.default.override {
-            inherit extraPackages;
-          })
-        ];
-
+      devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          nodejs
+          ags-bin
           corepack
+          nixfmt-rfc-style
+          nodejs
         ];
       };
+
+      formatter.${system} =
+        with pkgs;
+        writeShellScriptBin "format" ''
+          ${corepack}/bin/pnpm install --frozen-lockfile
+          ${corepack}/bin/pnpm run format:fix
+
+          ${nixfmt-rfc-style}/bin/nixfmt ./flake.nix
+        '';
     };
-  };
 }
